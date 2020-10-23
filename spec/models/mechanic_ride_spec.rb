0e04bed557db616a843f2379ffdb4583633f6bf9
Kiera Allen <kieraaallen@gmail.com>
2020-10-23 09:55:00 -0600

    Pass mechanic_ride relationships tests

--- spec/models/mechanic_ride_spec.rb
+++ spec/models/mechanic_ride_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 describe MechanicRide, type: :model do
   describe "relationships" do
     it { should belong_to :mechanic }
-    it { shoudl belong_to :ride }
+    it { should belong_to :ride }
   end
 end
